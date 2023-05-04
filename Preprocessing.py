7c97072a4dfd5195092aedbb954729b782970b64
contactless-healthcare <129282850+contactless-healthcare@users.noreply.github.com>
2023-05-04 13:30:44 +0800

    Add files via upload

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,107 +1,231 @@
+import os
+import shutil
 import numpy as np
-import config
+import scipy.signal as signal
 import librosa
-import librosa.display
+import Config
 import pickle
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import write
+import pywt
+from PyEMD import EMD
 
 
-def Data_Acq(fileName):
-    file = open(fileName, 'rb')
-    sample = pickle.load(file, encoding='latin1')
-    file.close()
+def Filter_Denoised(raw_audio, sample_rate, filter_order, filter_lowcut, filter_highcut, btype="bandpass"):
+    b, a = 0.0, 0.0
+    if btype == "bandpass":
+        b, a = signal.butter(filter_order, [filter_lowcut/(sample_rate/2), filter_highcut/(sample_rate/2)], btype=btype)
 
-    return sample
-
-
-
-def Wav2Segments(dataDir, labelDir):
-    def SlidingWindows2Segments(CryList, raw_audio, sample_rate, slidingLengthWindows, overlap, start=0):
-        slidingLengthWindows = int(slidingLengthWindows * sample_rate)  # number of frame
-        overlap = int(overlap * sample_rate)  # number of overlap
-
-        sample = {
-            "data": [],
-            "label": []
-        }
-        while start < len(raw_audio):
-            end = int(start + slidingLengthWindows)
-            # Data Acq
-            if end > len(raw_audio):
-                data = raw_audio[start: -1]
-                padding = np.zeros((end-len(raw_audio)+1,))
-                data = np.concatenate((data, padding))
-            else:
-                data = raw_audio[start: end]
-
-            # Label set
-            label = 1       # Cry - 0, No-Cry - 1
-            frameWork = [i for i in range(start, end)]
-            if len(list(set(frameWork).intersection(set(CryList)))) != 0:
-                label -= 1
-
-            # 存储
-            sample["data"].append(data)
-            sample["label"].append(label)
-
-            start += overlap
-
-        sample["data"] = np.array(sample["data"])
-        sample["label"] = np.array(sample["label"])
-
-        return sample
+    if btype == "highpass":
+        b, a = signal.butter(filter_order, filter_lowcut, btype=btype, fs=sample_rate)
 
 
-    raw_audio, sample_rate = librosa.load(path=dataDir, sr=config.audioSampleRate)
+    audio = signal.lfilter(b, a, raw_audio)
 
-    # Label from file
-    CryList = None
-    f = open(f"{labelDir}", "r")
-    res = f.readlines()
-    for cur in res:
+    return audio
+
+
+
+def Wavelet_Denoise(signal, wavelet='db4', level=1, threshold=None, mode='soft'):
+
+    coeffs = pywt.wavedec(signal, wavelet, level=level)
+    if threshold is None:
+        sigma = np.median(np.abs(coeffs[-1])) / 0.6745
+        threshold = sigma * np.sqrt(2 * np.log(len(signal)))
+
+    for i in range(1, len(coeffs)):
+        coeffs[i] = pywt.threshold(coeffs[i], threshold, mode=mode)
+
+    denoised_signal = pywt.waverec(coeffs, wavelet)
+    return denoised_signal
+
+
+
+def EMD_Denosing(data):
+    # Ref: A Lightweight CNN Model for Detecting Respiratory Diseases
+    # From Lung Auscultation Sounds Using EMD-CWT-Based Hybrid Scalogram
+    emd = EMD()
+    emd.emd(data, max_imf=9)
+    imfs, res = emd.get_imfs_and_residue()
+
+    goal_index = 0
+    correct = -10000
+    for imf_i in range(len(imfs)):
+        temp = np.correlate(data, imfs[imf_i])[0]
+        if temp > correct:
+            correct = temp
+            goal_index = imf_i
+    emd_sig = imfs[goal_index]
+    return emd_sig
+
+
+
+def Padding(data, sample_rate, respiratory_cycle, padding_more):
+
+    if len(data) == (sample_rate * respiratory_cycle):
+        return data  # the duration of data is equal to the length of the demand
+    else:
+        padding = None
+        if padding_more == "zero":
+            padding = np.zeros((data.shape[0]))
+        elif padding_more == "sample":
+            padding = data.copy()
+
+        while True:  # 反复拼接
+            data = np.concatenate([data, padding])
+            if len(data) > (sample_rate * respiratory_cycle):
+                data = data[:int(sample_rate * respiratory_cycle)]
+
+            if len(data) == (sample_rate * respiratory_cycle):
+                return data
+
+
+
+def Segmentation(audio, label_dir, sample_rate, respiratory_cycle, overlap, padding_more, show_function=False):
+    samples = {
+        "signal": [],
+        "label": [],
+    }
+
+    filelabel = open(f"{label_dir}", "r")
+    res = filelabel.readlines()
+    filelabel.close()
+
+    for i, cur in enumerate(res):
         cur = cur.strip("\n")
-        lStart, lEnd, lSet = cur.split("\t")
+        lStart, lEnd, lcrackle, lwheeze = cur.split("\t")
+        lStart = float(lStart)
+        lEnd = float(lEnd)
+        lcrackle = int(lcrackle)
+        lwheeze = int(lwheeze)
 
-        lStart = int(float(lStart) * config.audioSampleRate)
-        lEnd = int(float(lEnd) * config.audioSampleRate)
+        # Label Construction
+        label = None  # normal - 0, crackle - 1, wheezes - 2, both - 3
+        if lcrackle == 0 and lwheeze == 0:
+            label = Config.normal
+        elif lcrackle == 1 and lwheeze == 0:
+            label = Config.crackle
+        elif lcrackle == 0 and lwheeze == 1:
+            label = Config.wheezes
+        elif lcrackle == 1 and lwheeze == 1:
+            label = Config.both
 
-        if int(lSet) == 0:
-            if CryList is None:
-                CryList = np.array([i for i in range(lStart, lEnd)])
-            else:
-                CryList = np.concatenate((CryList, np.array([i for i in range(lStart, lEnd)])))
-    if CryList is None:
-        CryList = []
+        # Data Construction
+        while lStart < lEnd:
+            temp_start = lStart
+            temp_end = temp_start + respiratory_cycle
+            if temp_end > lEnd:
+                temp_end = lEnd
 
-    # Sliding windows to traim audio
-    sample = SlidingWindows2Segments(CryList, raw_audio, sample_rate, config.slidingWindows, config.overlap, start=0)
+            if (lEnd - lStart) < (Config.respiratory_cycle // 2):
+                break
 
-    # Save
-    saveDir = dataDir.split(".")[0] + ".dat"
-    file = open(saveDir, 'wb')
-    pickle.dump(sample, file)
-    file.close()
+            temp_start = int(temp_start * sample_rate)
+            temp_end = int(temp_end * sample_rate)
 
-    return sample
+            temp_data = Padding(audio[temp_start:temp_end], sample_rate, respiratory_cycle, padding_more)
+
+            if show_function:
+                plt.plot(temp_data)
+                plt.show()
+
+            samples["signal"].append(temp_data)
+            samples["label"].append(label)
+
+            lStart += overlap
+
+    return samples
 
 
 
+def Diagnosis_label(subject, diagnosis_file_dir):
+    filelabel = open(f"{diagnosis_file_dir}", "r")
+    res = filelabel.readlines()
+    filelabel.close()
 
-def main():
-    dataDir = f"Data\\Crying_Github.wav"
-    labelDir = f"Data\\Crying_Github.txt"
-    Wav2Segments(dataDir, labelDir)
+    diagosis_label_list = {}
+    for i, cur in enumerate(res):
+        cur = cur.strip("\n")
+        subjectID, diagnosis = cur.split("\t")
 
-    # sampleDir = f"Data\\Crying_Github.dat"
-    # sample = Data_Acq(sampleDir)
-    # print(sample)
+        if diagnosis == "URTI":
+            diagnosis = Config.URTI
+        elif diagnosis == "Healthy":
+            diagnosis = Config.Healthy
+        elif diagnosis == "Asthma":
+            diagnosis = Config.Asthma
+        elif diagnosis == "COPD":
+            diagnosis = Config.COPD
+        elif diagnosis == "LRTI":
+            diagnosis = Config.LRTI
+        elif diagnosis == "Bronchiectasis":
+            diagnosis = Config.Bronchiectasis
+        elif diagnosis == "Pneumonia":
+            diagnosis = Config.Pneumonia
+        elif diagnosis == "Bronchiolitis":
+            diagnosis = Config.Bronchiolitis
+
+        diagosis_label_list[subjectID] = diagnosis
+
+    return diagosis_label_list[subject]
+
+
+
+def Preprocessing(dir, preprocessed_dir_savesamples):
+    print('start Preprcessing')
+
+    # Make dir
+    if os.path.exists(f"{preprocessed_dir_savesamples}") and Config.save_for_preprocessing_and_feature_extraction:
+        shutil.rmtree(f"{preprocessed_dir_savesamples}")
+        os.makedirs(f"{preprocessed_dir_savesamples}")
+    else:
+        os.makedirs(f"{preprocessed_dir_savesamples}")
+
+    for file_name in os.listdir(dir):
+        if ".wav" not in file_name:
+            continue
+
+        # path
+        data_dir = f"{dir}\\{file_name}"
+        label_dir = f"{dir}\\{file_name.split('.')[0]}.txt"
+
+        # load data
+        raw_audio, sample_rate = librosa.load(path=data_dir, sr=Config.sample_rate)
+
+        # Noise reduction method, filter
+        audio_data = Filter_Denoised(raw_audio, sample_rate, Config.filter_order,
+                                     Config.filter_lowcut, Config.filter_highcut, btype=Config.filter_btype)
+        # Candidate method
+        # ..........
+
+        # Segmentation - data & label
+        samples = Segmentation(audio_data, label_dir, sample_rate, Config.respiratory_cycle, Config.overlap, Config.padding_mode)
+        if samples["signal"] == []:
+            continue
+
+        # diagnosis label
+        subject = file_name[:3]
+        diagnosis_label = Diagnosis_label(subject, Config.diagnosis_file_dir)
+
+        # Save to the preprocessed_dir_savesamples
+        if Config.save_for_preprocessing_and_feature_extraction:
+            for i in range(len(samples["signal"])):
+                save_dir = preprocessed_dir_savesamples + '\\' + file_name.split('.')[0] + f"_{i}.dat"
+
+                temp = {
+                    "signal": samples["signal"][i],
+                    "label": samples["label"][i],
+                    "diagnosis": diagnosis_label
+                }
+                with open(save_dir, 'wb') as f:
+                    pickle.dump(temp, f)
+
+        print(f"{file_name} over")
 
 
 
 if __name__ == '__main__':
-    main()
-
-
-
+    Preprocessing(Config.raw_dir, Config.preprocessed_dir_savesamples)
 
 
 
--- README.md
+++ README.md
@@ -1,30 +1,32 @@
-This is code repository for the paper "A Scene Adaption Framework for Infant Cry Detection in Obstetrics".
+# deep-learning-for-lung-sound-analysis
+
+This is code repository for the paper "Deep learning in wireless stethoscope-based lung sound analysis".
+
+It is developed for lung sound recognition based on the Pytorch, Librosa [1], and pyaudioanalysis [2], 
+It modularizes the operation of deep learning, including the preprocessing, feature extraction, dataset splitting and classifier training.
 
 
-It is developed for Infant Cry detection in a new environment (including clinical setting, e.g. Obstetrics).
+For the demonstration, we uses ICBHI 2017 dataset [3] as the example. 
+
+There are six major .py files in this repo.
+
+  1. Config.py: It controls the required parameters in each process.
+  2. Preprocessing.py: It contains the basic noise reduction functions and data segmentation.
+  3. Feature extraction.py: It transforms the lung sound signal into an input suitable for the model, such as statistical features and spectrogram.
+  4. Data splitting.py: It divided the whole dataset into training and testing sets (in the subject-wise way).
+  5. Classifier_training_and_testing: It is used to train and test the model.
+  6. Model.deep learning.py: It stores the definitions of the model and the training and testing procedures.
 
 
-There are six major .py files in this repo, which sequential runs implement infant cry detection based on machine learning:
-
-    1. Config.py: It controls the required parameters in each process.
-    2. Preprocessing.py: It contains the data segmentation to split the raw record to the segmentation with the predetermined length.
-    3. Feature extraction.py: It transforms the lung sound signal into an input suitable for the model, such as statistical features and spectrogram.
-    4. Scene Simulation Data Augmentation.py: It realizes the scene simulation-based data augmentation for infant cry detection.
-    5. Train.py: It consists of two training phases, a supervised training phase based on labeled data from source domain and a unsupervised training phase (scene adaptation) based on unlabeled data from target domain.
-    6. Test Demo for record: It provides the demonstration of making predictions to raw records.
-    
-
-We provide the trained CNN model for more extensive validation.
+To build a model for lung sound analysis, the 2-5 .py files should be performed sequentially, 
+and the Config.py and Model.deep learning.py is applied for custom Settings.
 
 
 
-Please cite the below paper if the code was used in your research or development.
-    
-    @inproceedings{
-        title={A Scene Adaption Framework for Infant Cry Detection in Obstetrics},
-        author={Huang, Dongmin and Ren, Lirong and Lu, Hongzhou and Wang, Wenjin}, 
-        booktitle={2023 45th Annual International Conference of the IEEE Engineering in Medicine and Biology Society (EMBC)},
-        year={2023}
-    }
+[1] McFee, B., Raffel, C., Liang, D., Ellis, D. P., McVicar, M., Battenberg, E., & Nieto, O. (2015, July). librosa: Audio and music signal analysis in python. In Proceedings of the 14th python in science conference (Vol. 8, pp. 18-25).
+
+[2] Giannakopoulos, T. (2015). pyaudioanalysis: An open-source python library for audio signal analysis. PloS one, 10(12), e0144610.
+
+[3] Rocha, B. M., Filos, D., Mendes, L., Serbes, G., Ulukaya, S., Kahya, Y. P., ... & De Carvalho, P. (2019). An open access database for the evaluation of respiratory sound classification algorithms. Physiological measurement, 40(3), 035001.
 
 
